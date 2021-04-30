fd67432f83caac52e26ecd2c9a84782f797922fa
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-01 00:47:03 +0900

    [Fix]createアクションのメソッド修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.build(task_params)
+    @task = Task.create(task_params)
     #@task = current_user.tasks.build(task_params)
     if params[:back]
       render :new
