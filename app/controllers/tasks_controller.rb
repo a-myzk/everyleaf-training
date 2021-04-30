76bd0e0cb6efaa67ccac7d15b36ebe9621e1c4d8
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-04-30 17:49:37 +0900

    [Add]タスク新規投稿ページを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,34 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update destroy ]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    #@task = current_user.tasks.build(task_params)
+    if params[:back]
+      render :new
+    else
+      respond_to do |format|
+        if @task.save
+          #ConfirmMailer.confirm_mail(@task).deliver
+          format.html { redirect_to @task, notice: "タスクを新規登録しました" }
+          format.json { render :show, status: :created, location: @task }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @task.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
 
   def edit
@@ -23,6 +41,10 @@ class TasksController < ApplicationController
   end
 
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:title, :content)
   end
