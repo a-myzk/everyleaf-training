92582581bf2b882ea63b96be2cc683e79dd20cfd
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-06 02:10:45 +0900

    [Test]タスク一覧が降順に並んでいるかテスト

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task)
+    FactoryBot.create(:second_task) }
+  before do
+    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+    visit tasks_path
+    # FactoryBot.create(:task)
+    # FactoryBot.create(:second_task)
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -9,7 +17,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         # 2. 新規登録内容を入力する
         #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
         # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-        fill_in 'タスク名', with: 'task'
+        fill_in 'タスク名', with: 'task_title'
         # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'タスク詳細', with: 'task_content'
         # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
@@ -18,7 +26,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
         # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
         # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'task_title'
         expect(page).to have_content 'task_content'
       end
     end
@@ -27,13 +35,19 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
         # タスク一覧ページに遷移
-        visit tasks_path
         # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
         # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
         expect(page).to have_content 'task'
         # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+    # テスト内容を追加で記載する
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # ここに実装する
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'test_title2'
       end
     end
   end
