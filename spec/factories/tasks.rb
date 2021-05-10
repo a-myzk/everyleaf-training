37111ff5d3639e5bdb6c552ba75a1135a36d28da
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-10 22:49:44 +0900

    [Fix]要件のテスト部分修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title1' }
-    content { 'test_content1' }
+    title { 'task_title1' }
+    content { 'task_content1' }
   end
 
   factory :second_task, class: Task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title2' }
-    content { 'test_content2' }
+    title { 'task_title2' }
+    content { 'task_content2' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,29 +3,15 @@ RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) { FactoryBot.create(:task)
     FactoryBot.create(:second_task) }
   before do
-    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
     visit tasks_path
-    # FactoryBot.create(:task)
-    # FactoryBot.create(:second_task)
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        # 1. new_task_pathに遷移する（新規作成ページに遷移する）
-        # ここにnew_task_pathにvisitする処理を書く
         visit new_task_path
-        # 2. 新規登録内容を入力する
-        #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
-        # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'タスク名', with: 'task_title'
-        # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in 'タスク詳細', with: 'task_content'
-        # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-        # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
         click_on '登録する'
-        # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-        # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-        # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
         expect(page).to have_content 'task_title'
         expect(page).to have_content 'task_content'
       end
@@ -34,20 +20,13 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを作成
-        # タスク一覧ページに遷移
-        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
-        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
         expect(page).to have_content 'task'
-        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
-    # テスト内容を追加で記載する
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        # ここに実装する
-        task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'test_title2'
+        task_list = all('.task_list')
+        expect(task_list[0]).to have_content 'task_title2'
       end
     end
   end
