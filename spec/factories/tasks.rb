f8ac973f41885daa4dfd9b847e44ff439ff041d9
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-11 22:58:30 +0900

    [Update]終了期限のカラムを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,12 +3,13 @@ FactoryBot.define do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'task_title1' }
     content { 'task_content1' }
-    
+    deadline { 'task_deadline1' }
   end
 
   factory :second_task, class: Task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
     title { 'task_title2' }
     content { 'task_content2' }
+    deadline { 'task_deadline2' }
   end
 end
