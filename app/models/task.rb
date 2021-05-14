4d9615efce941e8482314c0c56a83b899c73872d
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-14 13:56:07 +0900

    [Update]ステータスの入力フォーム変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
-  enum status:{"未着手": "未着手", "着手中": "着手中", "完了": "完了"}
+  enum status:{未着手: 1, 着手中: 2, 完了: 3}
 end
