6148715b44f5028e4d01feb60aad5d1b01ed0ac6
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-17 21:31:06 +0900

    [Test]検索のテスト追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -44,7 +44,43 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
-  
+  describe 'タスク管理機能', type: :system do
+    describe '検索機能' do
+      before do
+        # 必要に応じて、テストデータの内容を変更して構わない
+        FactoryBot.create(:task, title: "task")
+        FactoryBot.create(:second_task, title: "second_task")
+      end
+      context 'タイトルであいまい検索をした場合' do
+        it "検索キーワードを含むタスクで絞り込まれる" do
+          # タスクの検索欄に検索ワードを入力する (例: task)
+          fill_in 'タスク名', with: 'task'
+          # 検索ボタンを押す
+          click_on '検索'
+          expect(page).to have_content 'task'
+        end
+      end
+      context 'ステータス検索をした場合' do
+        it "ステータスに完全一致するタスクが絞り込まれる" do
+          # ここに実装する
+          # プルダウンを選択する「select」について調べてみること
+          select '未着手', from: 'search_status'
+          click_on '検索'
+          expect(page).to have_content '未着手'
+        end
+      end
+      context 'タイトルのあいまい検索とステータス検索をした場合' do
+        it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+          # ここに実装する
+          fill_in 'タスク名', with: 'task'
+          select '未着手', from: 'search_status'
+          click_on '検索'
+          expect(page).to have_content 'task'
+          expect(page).to have_content '未着手'
+        end
+      end
+    end
+  end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
