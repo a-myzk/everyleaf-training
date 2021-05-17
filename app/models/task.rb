75b511ec4d37a14db2633f0f53c4c5c836231438
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-17 14:20:24 +0900

    [Update]statusの選択文言一部変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   validates :content, presence: true
   validates :expired_at, presence: true
   validates :status, presence: true
-  enum status: { 未着手: 1, 着手: 2, 完了: 3 }
+  enum status: { 未着手: 1, 着手中: 2, 完了: 3 }
   enum priority: { 低: 1, 中: 2, 高: 3 }
   scope :search_title, -> (search_title) { where("title LIKE ?", "%#{search_title}%") }
   scope :search_status, -> (search_status) { where(status: search_status)}
