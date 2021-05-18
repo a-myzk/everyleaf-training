ad6e21a1615c2aedf350cf1f418ff86dcbef1fb4
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-18 12:10:18 +0900

    [Update]検索結果が表示されるよう修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,11 +9,11 @@ class TasksController < ApplicationController
       @tasks = Task.all.order(priority: :DESC).page(params[:page]).per(PER)
     elsif params[:search]
       if params[:search_title].present? && params[:search_status].present?
-        @tasks = tasks.search_title(params[:search_title]).search_status(params[:search_status]).page(params[:page]).per(PER)
-      elsif  params[:search_title].present?
-        @tasks = tasks.search_title(params[:search_title]).page(params[:page]).per(PER)
+        @tasks = Task.search_title(params[:search_title]).search_status(params[:search_status]).page(params[:page]).per(PER)
+      elsif params[:search_title].present?
+        @tasks = Task.search_title(params[:search_title]).page(params[:page]).per(PER)
       elsif params[:search_status].present?
-        @tasks = tasks.search_status(params[:search_status]).page(params[:page]).per(PER)
+        @tasks = Task.search_status(params[:search_status]).page(params[:page]).per(PER)
       else
         @tasks = Task.all.order(created_at: :DESC).page(params[:page]).per(PER)
       end
