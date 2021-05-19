2a867ba1e2fff3e7b4634ae1b185c288f0495e86
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-19 13:32:05 +0900

    [Update]自分以外のユーザのマイページにアクセスしたらタスク一覧に遷移させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if current_user.id != @user.id
+      redirect_to tasks_path
+    end
   end
 
   private
