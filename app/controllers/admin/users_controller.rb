675b99ca4f7c396eff70d3d3ec2dd19c3bb47d79
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-19 18:33:42 +0900

    [Add]ユーザ詳細ページ実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,7 +19,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = user.new(user_params)
     if @user.save
-      redirect_to admin_users_path, notice:"ユーザー「#{@user.name}」を登録しました"
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました"
     else
       render :new
     end
