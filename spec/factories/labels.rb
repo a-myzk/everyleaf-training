b974b4d301b8c9e8cc51de78970f362686e8ea3a
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-26 01:22:18 +0900

    [Test]ラベル機能テスト用のデータ作成

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :label do
-    label_name { "MyString" }
+    label_name { 'label1' }
+  end
+
+  factory :second_label, class: Label do
+    label_name { 'label2' }
   end
 end
---
+++ spec/system/label_spec.rb
@@ -0,0 +1,2 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
