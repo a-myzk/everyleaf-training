b2a15314172f89d50d7d6ce7646a74217bf0cd7a
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-05-26 21:53:51 +0900

    [Update]日本語を指定できるように設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- 
+
 name = "taro"
-email = "taro000@a.com"
+email = "taro00000@a.com"
 password = "0000pppp"
 User.create!(
   name: name,
@@ -17,7 +17,7 @@ User.create!(
   admin: true
 )
 
-10.times do |nrdb|
+10.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "0000pppp"
